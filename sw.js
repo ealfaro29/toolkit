b1c9688ac517cf51f30c3094f7f3a0e7a22b43cf
Esteban Alfaro <esteban@sentientagency.io>
2026-08-18 08:27:23 -0600

    fix: add .nojekyll, custom 404.html, and cache-clearing service worker

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Self-clearing Service Worker to unregister all caches and old service workers
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
